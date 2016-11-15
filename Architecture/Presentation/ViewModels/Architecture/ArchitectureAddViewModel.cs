c2420e8364e4b5d5cacc8c7e763cff3f498368d8
cyber2370 <suppy2370@gmail.com>
2016-11-15 21:09:18 +0200

    Fixed architectureaddpage layout, architecture shell viewmodel

--- Architecture/Presentation/ViewModels/Architecture/ArchitectureAddViewModel.cs
+++ Architecture/Presentation/ViewModels/Architecture/ArchitectureAddViewModel.cs
@@ -1,8 +1,38 @@
-﻿using Arcitecture.Presentation.ViewModels.Common;
+﻿using System;
+using System.Windows.Input;
+using Architecture.Data.Repositories.Interfaces;
+using Architecture.Managers.Interfaces;
+using Arcitecture.Presentation.ViewModels.Common;
+using GalaSoft.MvvmLight.Command;
 
 namespace Architecture.Presentation.ViewModels.Architecture
 {
     public class ArchitectureAddViewModel : ViewModelBase
     {
+        private readonly IArchitecturesManager _architecturesManager;
+
+        private string _name;
+        private DateTime _createdDate;
+        private string _country;
+        private string _city;
+        private string _address;
+        private string _square;
+        private int _heigth;
+        private string _style;
+        private Data.Entities.Architect _architect;
+
+        public ArchitectureAddViewModel(IArchitecturesManager architecturesManager)
+        {
+            _architecturesManager = architecturesManager;
+
+            SaveCommand = new RelayCommand(Save);
+        }
+
+        public ICommand SaveCommand { get; }
+
+        private void Save()
+        {
+            
+        }
     }
 }
--- Architecture/Presentation/ViewModels/Architecture/ArchitectureMainViewModel.cs
+++ Architecture/Presentation/ViewModels/Architecture/ArchitectureMainViewModel.cs
@@ -1,14 +1,114 @@
-﻿using Arcitecture.Presentation.ViewModels.Common;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+using Architecture.Presentation.Helpers;
+using Architecture.Presentation.Models;
+using Arcitecture.Presentation.ViewModels.Common;
+using GalaSoft.MvvmLight.Command;
 
 namespace Architecture.Presentation.ViewModels.Architecture
 {
     public class ArchitectureMainViewModel : ViewModelBase
     {
-        public ArchitectureMainViewModel()
-        {
 
+        public class LeftMenuItem
+        {
+            public SymbolIcon Icon { get; set; }
+            public string Text { get; set; }
+
+            public PageKeys InnerPageKey { get; set; }
+            public object Params { get; set; }
         }
 
+        private ObservableCollection<LeftMenuItem> _bottomMenuItems;
 
+        private LeftMenuItem _selectedBottomMenuItem;
+        private Type _currentPageType;
+
+        public ArchitectureMainViewModel()
+        {
+            CreateBottomMenuItems();
+
+            SetDefaultSelectedMenuItem();
+        }
+
+        public ObservableCollection<LeftMenuItem> BottomMenuItems
+        {
+            get { return _bottomMenuItems; }
+            private set { Set(() => BottomMenuItems, ref _bottomMenuItems, value); }
+        }
+
+        public LeftMenuItem SelectedBottomMenuItem
+        {
+            get { return _selectedBottomMenuItem; }
+            set
+            {
+                _selectedBottomMenuItem = value;
+                
+                CurrentPageType = value.InnerPageKey.GetPageType();
+            }
+        }
+
+        public Type CurrentPageType
+        {
+            get { return _currentPageType; }
+            set { Set(() => CurrentPageType, ref _currentPageType, value); }
+        }
+
+        private void SetDefaultSelectedMenuItem()
+        {
+            SelectedBottomMenuItem = _bottomMenuItems.Single(i => i.InnerPageKey == PageKeys.ArchitectureSearch);
+        }
+
+        private void CreateBottomMenuItems()
+        {
+            BottomMenuItems = new ObservableCollection<LeftMenuItem>
+            {
+                new LeftMenuItem
+                {
+                    Text = "Поиск",
+                    Icon = new SymbolIcon(Symbol.Find),
+                    InnerPageKey = PageKeys.ArchitectureSearch
+                },
+                new LeftMenuItem
+                {
+                    Text = "Фильтрация",
+                    Icon = new SymbolIcon(Symbol.Filter),
+                    InnerPageKey = PageKeys.ArchitectureFilter
+                },
+                new LeftMenuItem
+                {
+                    Text = "Добавление",
+                    Icon = new SymbolIcon(Symbol.Add),
+                    InnerPageKey = PageKeys.ArchitectureAdd
+                },
+                new LeftMenuItem
+                {
+                    Text = "Отчеты",
+                    Icon = new SymbolIcon(Symbol.Document),
+
+                    //TODO: Set correct page
+                    InnerPageKey = PageKeys.ArchitectureAdd
+                },
+                new LeftMenuItem
+                {
+                    Text = "Статистика",
+                    Icon = new SymbolIcon(Symbol.List),
+
+                    //TODO: Set correct page
+                    InnerPageKey = PageKeys.ArchitectureAdd
+                },
+                new LeftMenuItem
+                {
+                    Text = "Ремонт",
+                    Icon = new SymbolIcon(Symbol.Repair),
+
+                    //TODO: Set correct page
+                    InnerPageKey = PageKeys.ArchitectureAdd
+                }
+            };
+        }
     }
 }
