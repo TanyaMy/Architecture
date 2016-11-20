bc73e6694c7e45c94cdb9b5ba9ae77b642c4490c
cyber2370 <suppy2370@gmail.com>
2016-11-20 12:43:29 +0200

    Inner AppBar fixes

--- Architecture/Presentation/ViewModels/Architecture/ArchitectureViewModel.cs
+++ Architecture/Presentation/ViewModels/Architecture/ArchitectureViewModel.cs
@@ -1,106 +1,48 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows.Input;
 using Windows.UI.Xaml.Controls;
 using Architecture.Presentation.Helpers;
 using Architecture.Presentation.Models;
 using Arcitecture.Presentation.ViewModels.Common;
+using GalaSoft.MvvmLight.Command;
 
 namespace Architecture.Presentation.ViewModels.Architecture
 {
     public class ArchitectureViewModel : ViewModelBase
     {
-
-        public class BottomMenuItem
-        {
-            public SymbolIcon Icon { get; set; }
-            public string Text { get; set; }
-
-            public PageKeys InnerPageKey { get; set; }
-            public object Params { get; set; }
-        }
-
-        private ObservableCollection<BottomMenuItem> _bottomMenuItems;
-
-        private BottomMenuItem _selectedBottomMenuItem;
         private Type _currentPageType;
 
         public ArchitectureViewModel()
         {
-            CreateBottomMenuItems();
+            NavTo = new RelayCommand<PageKeys>(NavigateTo);
 
-            SetDefaultSelectedMenuItem();
+            SetDefaultInnerPage();
         }
 
-        public ObservableCollection<BottomMenuItem> BottomMenuItems
-        {
-            get { return _bottomMenuItems; }
-            private set { Set(() => BottomMenuItems, ref _bottomMenuItems, value); }
-        }
+        public ICommand NavTo { get; set; }
 
-        public BottomMenuItem SelectedBottomMenuItem
-        {
-            get { return _selectedBottomMenuItem; }
-            set
-            {
-                _selectedBottomMenuItem = value;
-                
-                CurrentPageType = value.InnerPageKey.GetPageType();
-            }
-        }
+        public PageKeys Main => PageKeys.ArchitectureMain;
+        public PageKeys Add => PageKeys.ArchitectureAdd;
+        public PageKeys Reports => PageKeys.ArchitectureReports;
+        public PageKeys Statistics => PageKeys.ArchitectureStatistics;
+        public PageKeys Repair => PageKeys.ArchitectureStatistics;
 
-        public Type CurrentPageType
+        public Type CurrentInnerPageType
         {
             get { return _currentPageType; }
-            set { Set(() => CurrentPageType, ref _currentPageType, value); }
+            set { Set(() => CurrentInnerPageType, ref _currentPageType, value); }
         }
 
-        private void SetDefaultSelectedMenuItem()
+        private void NavigateTo(PageKeys pageKey)
         {
-            SelectedBottomMenuItem = _bottomMenuItems.Single(i => i.InnerPageKey == PageKeys.ArchitectureMain);
+            CurrentInnerPageType = pageKey.GetPageType();
         }
 
-        private void CreateBottomMenuItems()
+        private void SetDefaultInnerPage()
         {
-            BottomMenuItems = new ObservableCollection<BottomMenuItem>
-            {
-                new BottomMenuItem
-                {
-                    Text = "Главная",
-                    Icon = new SymbolIcon(Symbol.Home),
-                    InnerPageKey = PageKeys.ArchitectureMain
-                },
-                new BottomMenuItem
-                {
-                    Text = "Добавление",
-                    Icon = new SymbolIcon(Symbol.Add),
-                    InnerPageKey = PageKeys.ArchitectureAdd
-                },
-                new BottomMenuItem
-                {
-                    Text = "Отчеты",
-                    Icon = new SymbolIcon(Symbol.Document),
-
-                    //TODO: Set correct page
-                    InnerPageKey = PageKeys.ArchitectureReports
-                },
-                new BottomMenuItem
-                {
-                    Text = "Статистика",
-                    Icon = new SymbolIcon(Symbol.List),
-
-                    //TODO: Set correct page
-                    InnerPageKey = PageKeys.ArchitectureStatistics
-                },
-                new BottomMenuItem
-                {
-                    Text = "Ремонт",
-                    Icon = new SymbolIcon(Symbol.Repair),
-
-                    //TODO: Set correct page
-                    InnerPageKey = PageKeys.ArchitectureStatistics
-                }
-            };
+            CurrentInnerPageType = PageKeys.ArchitectureMain.GetPageType();
         }
     }
 }
